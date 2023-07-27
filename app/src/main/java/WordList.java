8d66cc2ae446ebdd5e7e2cb940354f2201be6227
Ryan N <ryann66@cs.washington.edu>
2023-07-27 15:04:29 -0700

    Added Java logic, apart from word selection algorithm.

--- app/src/main/java/WordList.java
+++ app/src/main/java/WordList.java
@@ -19,7 +19,27 @@ public class WordList{
         }
     }
 
-    public void addConstraint(Constraint constraint) {
+    /**
+     * Removes any invalid words from the word list
+     * @param constraint the constraint to be placed upon all words in the word list
+     * @throws IllegalStateException if the word list is empty afterwards
+     */
+    public void addConstraint(Constraint constraint) throws IllegalStateException{
+        Iterator<String> iter = words.iterator();
+        while(iter.hasNext()){
+            if(constraint.invalid(iter.next())) iter.remove();
+        }
+        if(words.isEmpty()) throw new IllegalStateException("Empty word list");
+    }
 
+    /**
+     * Returns the best word that can be played
+     * @return the best word that can be played
+     * @throws IllegalStateException if the wordlist is empty
+     */
+    public String getBestWord(){
+        if(words.isEmpty()) throw new IllegalStateException();
+        //TODO: fancy algorithm
+        return null;
     }
 }
