62aa637b722deffafc3412a8bec19dd3d3e76755
ryann66 <ryannelson6@hotmail.com>
2023-07-27 20:22:01 -0700

    Various

--- app/src/main/java/com/example/wordlecheater/MainActivity.kt
+++ app/src/main/java/com/example/wordlecheater/MainActivity.kt
@@ -1,20 +1,12 @@
 package com.example.wordlecheater
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.wordlecheater.ui.theme.WordleCheaterTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        Text("Hello")
     }
 }
