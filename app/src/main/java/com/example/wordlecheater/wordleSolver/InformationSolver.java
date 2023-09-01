a7442f6f6362b7389ddaea83b9e28d37cc640806
ryann66 <ryannelson6@hotmail.com>
2023-09-01 10:54:02 -0700

    Implemented InformationSolver

--- app/src/main/java/com/example/wordlecheater/wordleSolver/InformationSolver.java
+++ app/src/main/java/com/example/wordlecheater/wordleSolver/InformationSolver.java
@@ -1,5 +1,7 @@
 package com.example.wordlecheater.wordleSolver;
 
+import static java.lang.Double.NaN;
+
 /**
  * Implementation of WordleSolver that picks a word from the list of possible answers algorithmically
  */
@@ -13,6 +15,8 @@ public class InformationSolver extends AbstractWordleSolver{
     public String getBestWord() {
         if(noWords()) throw new IllegalStateException("No words remaining");
         //todo: ensure that picked word is a possible answer if possibleAnswers.size() is too small
+
+        if(possibleAnswers.size() <= 2) return possibleAnswers.get(0);
 
         String bestWord = possibleGuesses.get(0);
         double bestInfo = evaluateWord(possibleGuesses.get(0));
@@ -29,12 +33,22 @@ public class InformationSolver extends AbstractWordleSolver{
     }
 
     // evaluates the given word and returns the expected information from it
+    // Algorithm loosely based on 3Blue1Brown's solution in https://www.youtube.com/watch?v=v68zYyaEmEA
     private double evaluateWord(String word){
+        //get the frequencies of each possible guess pattern that wordle can return
         int[] possiblePatterns = new int[(int)Math.pow(3, Wordle.WORD_LENGTH)];
         for(String possibleAnswer : possibleAnswers){
             possiblePatterns[Wordle.guess(possibleAnswer, word).hashCode()]++;
         }
-        //todo get average value of possiblePatterns
-        //todo return value
+        //calculate the information gained from each pattern and the probability of the guess being selected
+        //sum the information for each guess
+        double informationSum = 0;
+        for(int i = 0; i < possiblePatterns.length; i++){
+            double probability = ((double)possiblePatterns[i]) / possibleAnswers.size();
+            if(probability == 0) continue;//no information to be gained from impossible outcome
+            double information = probability * -(Math.log(probability) / Math.log(2));
+            informationSum += information;
+        }
+        return informationSum;
     }
 }
